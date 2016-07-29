05e478c1655d406a87371c8ce53f79a84bca5afe
sow4063 <sow4063@gmail.com>
2016-07-29 17:23:11 +0900

    2016.7.29. PaneraClient initial version.

---
+++ Documents/workspace/PaneraCli/src/PaneraCli.java
@@ -0,0 +1,127 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PaneraCli
+{
+	private static Socket sock;
+	private static String fileName;
+	private static BufferedReader stdin;
+	private static PrintStream os;
+	
+	public static void main(String args[]) throws IOException {
+		try {
+			sock = new Socket("localhost", 7405);
+			stdin = new BufferedReader(new InputStreamReader(System.in));
+		}
+		catch(Exception e) {
+			System.err.println("Can not connect to server, try again later.");
+			System.exit(1);
+		}
+		
+		os = new PrintStream(sock.getOutputStream());
+		
+		try {
+			switch(Integer.parseInt(selectAction())) {
+			case 1:
+				os.println("1");
+				sendFile();
+				break;
+			case 2:
+				os.println("2");
+				System.err.println("Enter file name: ");
+				fileName = stdin.readLine();
+				os.println(fileName);
+				receiveFile(fileName);
+				break;
+			default:
+					break;
+			}
+		}
+		catch(Exception e) {
+			System.err.println("not valid input");
+		}
+		
+		sock.close();
+	}
+	
+	public static String selectAction() throws IOException {
+		System.out.println("1. Send File.");
+		System.out.println("2. Receive File.");
+		System.out.print("\nMake Selection.");
+		
+		return stdin.readLine();
+	}
+	
+	public static void sendFile() {
+		
+        try {
+            System.err.print("Enter file name: ");
+            fileName = stdin.readLine();
+
+            File myFile = new File(fileName);
+            byte[] mybytearray = new byte[(int) myFile.length()];
+
+            FileInputStream fis = new FileInputStream(myFile);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            //bis.read(mybytearray, 0, mybytearray.length);
+
+            DataInputStream dis = new DataInputStream(bis);
+            dis.readFully(mybytearray, 0, mybytearray.length);
+            dis.close();
+
+            OutputStream os = sock.getOutputStream();
+
+            //Sending file name and file size to the server
+            DataOutputStream dos = new DataOutputStream(os);
+            dos.writeUTF(myFile.getName());
+            dos.writeLong(mybytearray.length);
+            dos.write(mybytearray, 0, mybytearray.length);
+            dos.flush();
+            System.out.println("File "+fileName+" sent to Server.");
+        } catch (Exception e) {
+            System.err.println("File does not exist!");
+        }
+    }
+
+    public static void receiveFile(String fileName) {
+    	
+        try {
+        	
+            int bytesRead;
+            
+            InputStream in = sock.getInputStream();
+
+            DataInputStream clientData = new DataInputStream(in);
+
+            fileName = clientData.readUTF();
+            OutputStream output = new FileOutputStream(("received_from_server_" + fileName));
+            long size = clientData.readLong();
+            byte[] buffer = new byte[1024];
+            while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
+                output.write(buffer, 0, bytesRead);
+                size -= bytesRead;
+            }
+
+            output.close();
+            in.close();
+
+            System.out.println("File "+fileName+" received from Server.");
+        } 
+        catch (IOException ex) {
+            Logger.getLogger("PaneraClient").log(Level.SEVERE, null, ex);
+        }
+    }
+}
