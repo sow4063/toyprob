d3d4651ec64f3a8f79c897c545b0cf38602925c4
sow4063 <sow4063@gmail.com>
2016-09-29 09:12:50 +0900

    movingzeros

---
+++ Documents/dev/toyprob/js/movingZeros.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/moving-zeros-to-the-end/train/javascript
+
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end,
+preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+var moveZeros = function (arr) {
+  return arr.filter(function(value){return value !== 0}).concat(arr.filter(function(value) {return value === 0;}));
+};
+
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"], [false,1,1,2,1,3,"a",0,0] );
