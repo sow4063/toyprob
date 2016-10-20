9367c7a8f1b270376bc42689315c01f6b662e2b5
sow4063 <sow4063@gmail.com>
2016-10-21 00:37:28 +0900

    sort string alphabetical order update

--- Documents/dev/toyprob/js/longestPalindrome.js
+++ Documents/dev/toyprob/js/longestPalindrome.js
@@ -54,3 +54,14 @@ function longestPalindrome( str ) {
 };
 
 module.exports = longestPalindrome;
+
+// best practice
+var longestPalindrome = function(s) {
+  if (!s) return 0;
+  for (let c = s.length; c > 0; c--) {
+    for (let i = 0; i <= s.length - c; i++) {
+      var check = s.substr(i, c);
+      if (check === check.split("").reverse().join("")) return c;
+    }
+  }
+};
