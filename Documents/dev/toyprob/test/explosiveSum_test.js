421b0f45cd180a2f0f230bb0b2da860820971f52
sow4063 <sow4063@gmail.com>
2016-10-24 07:40:20 +0900

    update for explosive sum

--- Documents/dev/toyprob/test/explosiveSum_test.js
+++ Documents/dev/toyprob/test/explosiveSum_test.js
@@ -11,4 +11,11 @@ describe('Test for explosive Sum', function() {
     assert.equal(sum(4), 5);
     assert.equal(sum(5), 7);
   });
+
+  it('should work for big sum', function() {
+    assert.equal(sum(10), 42);
+    assert.equal(sum(50), 204226);
+    assert.equal(sum(80), 15796476);
+    assert.equal(sum(100), 190569292);
+  });
 });
