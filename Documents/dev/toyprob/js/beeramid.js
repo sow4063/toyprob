901049c73034ea9202b0500022dfc85f1fd4207c
sow4063 <sow4063@gmail.com>
2016-10-30 11:19:09 +0900

    beeramid

--- Documents/dev/toyprob/js/beeramid.js
+++ Documents/dev/toyprob/js/beeramid.js
@@ -9,8 +9,8 @@ paid you a referral bonus. Awesome!
  using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can.
  And then probably drink those beers, because let's pretend it's Friday too.
 
-A beer can pyramid will square the number of cans in each level - 1 can in the top level,
-4 in the second, 9 in the next, 16, 25...
+A beer can pyramid will square the number of cans in each level -
+1 can in the top level, 4 in the second, 9 in the next, 16, 25...
 
 Complete the beeramid function to return the number of complete levels of
 a beer can pyramid you can make, given the parameters of:
@@ -28,6 +28,28 @@ beeramid(5000, 3); // should === 16
 // Returns number of complete beeramid levels
 var beeramid = function(bonus, price) {
 
+  if( bonus < price )
+    return 0;
+
+  var n = Math.floor(bonus / price);
+
+  var i = 0;
+  var sum = 0;
+  while( n > sum ) {
+    sum += Math.pow(++i,2);
+  }
+
+  return n === sum ? i : i - 1;
+
 };
 
 module.exports = beeramid;
+
+// best practice
+var beeramid = function(bonus, price) {
+  var beers = Math.floor(bonus / price), levels = 0;
+  while(beers >= ++levels * levels) {
+    beers -= levels * levels;
+  }
+  return levels - 1;
+}
