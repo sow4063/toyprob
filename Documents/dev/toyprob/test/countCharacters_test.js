65767a39b50d1112a003ce74c6679b1bc1ea8ec7
sow4063 <sow4063@gmail.com>
2016-11-12 11:34:49 +0900

    count characters : test case

--- Documents/dev/toyprob/test/countCharacters_test.js
+++ Documents/dev/toyprob/test/countCharacters_test.js
@@ -4,7 +4,7 @@ var count = require('../js/countCharacters.js');
 
 describe('Test for count characters in string', function() {
   it('should equal', function() {
-    assert.equal(count(''), {});
-    assert.equal(count('aba'), { 'a': 2, 'b': 1 });
+    assert.deepEqual(count(''), {});
+    assert.deepEqual(count('aba'), { 'a': 2, 'b': 1 });
   });
 });
