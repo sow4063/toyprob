5bb622758cc5efc2bcb8db211249b41763a15c24
sow4063 <sow4063@gmail.com>
2016-11-14 20:30:05 +0900

    large factorial

--- Documents/dev/toyprob/js/largeFactorials.js
+++ Documents/dev/toyprob/js/largeFactorials.js
@@ -18,51 +18,65 @@ Note: 0! is always equal to 1. Negative values should return null or an empty st
 For more on Factorials : http://en.wikipedia.org/wiki/Factorial
 */
 
-// This function finds factorial of large numbers and prints them
-function factorial(n)
-{
-    var res = new Array(500);
+function factorial(n) {
+  var result = new Array(1000);
+  result[0] = 1;
+  var res_size = 1;
 
-    // Initialize result
-    res[0] = 1;
-    var res_size = 1;
+  for( var i = 2; i <= n; i++ ) {
+    res_size = multiply( i, result, res_size );
+  }
 
-    // Apply simple factorial formula n! = 1 * 2 * 3 * 4...*n
-    for (var x=2; x<=n; x++)
-        res_size = multiply(x, res, res_size);
+  return result.reverse().join('');
+};
 
-    console.log('Factorial of given number is ');
-    for (var i=res_size-1; i>=0; i--)
-        console.log( res[i] );
-
-    return res[res.length-1];
-}
-
-// This function multiplies x with the number represented by res[].
-// res_size is size of res[] or number of digits in the number represented
-// by res[]. This function uses simple school mathematics for multiplication.
-// This function may value of res_size and returns the new value of res_size
 function multiply( x, res, res_size ) {
-  var carry = 0;  // Initialize carry
+  var carry = 0;
 
-  // One by one multiply n with individual digits of res[]
-  for (var i=0; i<res_size; i++)
-  {
-      var prod = res[i] * x + carry;
-      res[i] = prod % 10;  // Store last digit of 'prod' in res[]
-      carry  = parseInt(prod/10);    // Put rest in carry
+  for( var i = 0; i < res_size; i++ ) {
+    var prod = res[i] * x + carry;
+    res[i] = prod % 10;
+    carry = parseInt( prod / 10 );
   }
 
-  // Put carry in res and increase result size
-  while (carry)
-  {
-      res[res_size] = carry%10;
-      carry = parseInt(carry/10);
-      res_size++;
+  while( carry ) {
+    res[res_size] = carry % 10;
+    carry = parseInt( carry / 10 );
+    res_size++;
   }
+
   return res_size;
-}
+};
 
 module.exports = factorial;
 
 // best practice
+function BigInt(n) {
+  this.digits = Array.isArray(n) ? n : n.toString().split('').reverse().map(Number);
+
+  this.mulInt = function(n) {
+    var digits = this.digits;
+    var result = [];
+    var carry = 0;
+    for(var i = 0; i < digits.length; i++) {
+      var prod = carry + n * digits[i];
+      var d = prod % 10;
+      carry = (prod - d) / 10;
+      result.push(d);
+    }
+    if (carry > 0)
+      result.push.apply(result, carry.toString().split('').reverse().map(Number));
+    return new BigInt(result);
+  }
+
+  this.toString = function() {
+    return this.digits.reverse().join('');
+  }
+}
+
+function factorial(n) {
+  var result = new BigInt(1);
+  for(var i = 1; i <= n; i++)
+    result = result.mulInt(i);
+  return result.toString();
+}
--- Documents/dev/toyprob/test/largeFactorials_test.js
+++ Documents/dev/toyprob/test/largeFactorials_test.js
@@ -10,13 +10,13 @@ describe('Large Factorials', function() {
   it('Basic Tests', function() {
     assert.equal(factorial(5), '120', '5!');
   });
-  
-  // it('Basic Tests', function() {
-  //   assert.equal(factorial(9), '362880', '9!');
-  // });
-  //
-  // it('Basic Tests', function() {
-  //   assert.equal(factorial(15), '1307674368000', '15!');
-  // });
+
+  it('Basic Tests', function() {
+    assert.equal(factorial(9), '362880', '9!');
+  });
+
+  it('Basic Tests', function() {
+    assert.equal(factorial(15), '1307674368000', '15!');
+  });
 
 });
