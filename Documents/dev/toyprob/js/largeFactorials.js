c8d4a3e7e18431f81569919ca4ab3e3086c9f312
sow4063 <sow4063@gmail.com>
2016-11-14 20:35:10 +0900

    large factorial : clever

--- Documents/dev/toyprob/js/largeFactorials.js
+++ Documents/dev/toyprob/js/largeFactorials.js
@@ -80,3 +80,17 @@ function factorial(n) {
     result = result.mulInt(i);
   return result.toString();
 }
+
+// clever
+function factorial(n) {
+  var res = [1];
+  for (var i = 2; i <= n; ++i) {
+    var c = 0;
+    for (var j = 0; j < res.length || c !== 0; ++j) {
+      c += (res[j] || 0) * i;
+      res[j] = c % 10;
+      c = Math.floor(c / 10);
+    }
+  }
+  return res.reverse().join("");
+}
