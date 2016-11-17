af43739c895c10d9246d910dbb4e33da60a79040
sow4063 <sow4063@gmail.com>
2016-11-17 20:45:14 +0900

    range extraction

--- Documents/dev/toyprob/js/rangeExtraction.js
+++ Documents/dev/toyprob/js/rangeExtraction.js
@@ -10,7 +10,7 @@ A format for expressing an ordered list of integers is to use a comma separated 
 The range includes all integers in the interval including both endpoints.
 It is not considered a range unless it spans at least 3 numbers. For example ("12, 13, 15-17")
 
-Complete the solution so that it takes a list of integers in increasing order and 
+Complete the solution so that it takes a list of integers in increasing order and
 returns a correctly formatted string in the range format.
 
 Example:
@@ -20,7 +20,77 @@ solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20
 */
 
 function solution(list){
- // TODO: complete solution
+  var results = [];
+  var temp = [];
+
+  for( var i = 0; i < list.length; i++ ) {
+    if( i === 0 || list[i] === list[i - 1] + 1 ) {
+      temp.push(list[i]);
+    }
+    else {
+      // divide range
+      if( temp.length < 3 ) {
+        for( var j = 0; j < temp.length; j++ ) {
+          results.push(temp[j]);
+        }
+      }
+      else {
+        results.push( temp[0] + '-' + temp[temp.length - 1] );
+      }
+
+      temp = [];
+      temp.push(list[i]);
+
+    }
+
+  }
+
+  // divide range
+  if( temp.length < 3 ) {
+    for( var j = 0; j < temp.length; j++ ) {
+      results.push(temp[j]);
+    }
+  }
+  else {
+    results.push( temp[0] + '-' + temp[temp.length - 1] );
+  }
+
+  return results.join(',');
 };
 
 module.exports = solution;
+
+// best practice
+function solution(individualIntegers) {
+  return individualIntegers
+    .reduce(splitIntoRanges, [])
+    .map(convertToRange)
+    .join(",");
+}
+
+function splitIntoRanges(ranges, number) {
+  if (!ranges.length) {
+    ranges.push([number]);
+    return ranges;
+  }
+
+  var lastRange = ranges[ranges.length - 1];
+  var lastNumber = lastRange[lastRange.length - 1];
+
+  number === lastNumber + 1 ? lastRange.push(number) : ranges.push([number]);
+  return ranges;
+}
+
+function convertToRange(range) {
+  return range.length < 3 ? range.join(",") : range[0] + "-" + range[range.length - 1];
+}
+
+// clever
+function solution(list){
+   for(var i = 0; i < list.length; i++){
+      var j = i;
+      while(list[j] - list[j+1] == -1) j++;
+      if(j != i && j-i>1) list.splice(i, j-i+1, list[i] +'-'+list[j]);
+  }
+  return list.join();
+}
