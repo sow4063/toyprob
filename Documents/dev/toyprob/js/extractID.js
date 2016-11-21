177110eb9e21837f11c20c3f1560ae71142b388b
sow4063 <sow4063@gmail.com>
2016-11-21 10:53:25 +0900

    extract ids : best practice

--- Documents/dev/toyprob/js/extractID.js
+++ Documents/dev/toyprob/js/extractID.js
@@ -36,3 +36,18 @@ function extractIds(data) {
 };
 
 module.exports = extractIds;
+
+// best practice
+function extractIds(data) {
+  let ids  = []
+  if ('id' in data) ids.push(data.id);
+  if ('items' in data) data.items.forEach(item => ids = ids.concat(extractIds(item)));
+  return ids;
+}
+
+// clever
+function extractIds(data){
+  return [].concat.apply([], Object.keys(data).map(function(item) {
+    return item === 'id' ? data[item] : extractIds(data[item]);
+  }));
+}
