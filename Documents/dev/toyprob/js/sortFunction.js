c34d611c38cf51a043e18336034b9bbd98e15515
sow4063 <sow4063@gmail.com>
2016-11-27 09:51:56 +0900

    custom sort function

---
+++ Documents/dev/toyprob/js/sortFunction.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/custom-sort-function/train/javascript
+
+/*
+Complete the sort function so that it returns the items passed into it in alphanumerical order.
+Conveniently enough, the standard array sort method has been disabled for you
+so that you are forced to create your own.
+
+Example:
+
+sort([1,3,2]) // should return [1,2,3]
+*/
+
+function sort(items){
+  // create your custom sort logic
+};
+
+module.exports = sort;
